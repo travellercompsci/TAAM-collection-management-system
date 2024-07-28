3dc3af84dcae6636b35e8acab7f4257d24840a45
raifa-s <raifa.sarwar29@gmail.com>
2024-07-28 02:22:39 -0400

    fix: replaced empty file with AddCollectionActivity.java

---
+++ app/src/main/java/com/example/taamcms/AddCollectionActivity.java
@@ -0,0 +1,44 @@
+package com.example.taamcms;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class AddCollectionActivity extends Fragment {
+    Button buttonAdd;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view2 = inflater.inflate(R.layout.activity_collection_page, container, false);
+
+        buttonAdd = view2.findViewById(R.id.add_button);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new AddScreenActivity());
+            }
+        });
+
+        return view2;
+    }
+
+    protected void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
