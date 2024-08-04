137d87e75ef844a325a06cb6957074b9dc152310
raifa-s <raifa.sarwar29@gmail.com>
2024-08-04 13:57:47 -0400

    feat: added the login screen presenter component

---
+++ app/src/main/java/com/example/taamcms/LoginScreenPresenter.java
@@ -0,0 +1,44 @@
+package com.example.taamcms;
+
+public class LoginScreenPresenter {
+
+    LoginScreenView view;
+    LoginScreenModule model;
+
+    public LoginScreenPresenter(LoginScreenView view, LoginScreenModule model) {
+        this.view = view;
+        this.model = model;
+    }
+
+    public void handleLogin(String username, String password) {
+        if (username.equals("")) {
+            view.showUsernameErr(username);
+        }
+        else if (password.equals("")){
+            view.showPasswordErr(password);
+        }
+        else {
+            model.isValidCredentials(username, password, new CredentialValidationCallback() {
+                @Override
+                public void isSuccessful() {
+
+                }
+
+                @Override
+                public void invalidUsername() {
+
+                }
+
+                @Override
+                public void invalidPassword() {
+
+                }
+
+                @Override
+                public void databaseError() {
+
+                }
+            });
+        }
+    }
+}
